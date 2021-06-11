8e8cb48a1370c2f0d05cf2bfa575942dd5dcd62d
Ron <54092507+snwlvl@users.noreply.github.com>
2021-06-11 13:39:01 -0600

    Update crt.sh

--- crt.sh
+++ crt.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#author: ron @snwlol
+#author: ron @snwlvl
 
 curl -s https://crt.sh/?q=%.$1 > /tmp/curl.out
 cat /tmp/curl.out | grep $1 | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | httprobe  | sort -u > $1-sublist.txt
